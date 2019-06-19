504620f6033993be977b0ffe8faef4bd36bf6fec
mtummal1 <34357102+mtummal1@users.noreply.github.com>
2019-06-19 10:42:58 -0700

    Update try_excepy_finally.py

--- try_excepy_finally.py
+++ try_excepy_finally.py
@@ -1,1 +1,19 @@
 
+def abc():
+  try:
+
+        1/0
+
+  except TypeError:
+
+          print("caught TypeError")
+  except:
+
+         print('caught exception')
+
+  finally:
+
+         print('finally')       
+
+
+
