71f139f00f581ab0db24306751f8c4f96ca6d565
mtummal1 <34357102+mtummal1@users.noreply.github.com>
2019-06-23 08:47:11 -0700

    Create BubbleSort.py

---
+++ Assignment_3/BubbleSort.py
@@ -0,0 +1,25 @@
+ 
+#declare an array to perform the bubble sorted
+arr = [12, 2, 25, 11, 1, 14, 15]
+ 
+def bubbleSort(arr):
+    n = len(arr)
+ 
+    # Traverse through all array elements
+    for i in range(n):
+ 
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+ 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+ 
+
+bubbleSort(arr)
+ 
+print ("Sorted array is:")
+for i in range(len(arr)):
+    print ("%d" %arr[i])
